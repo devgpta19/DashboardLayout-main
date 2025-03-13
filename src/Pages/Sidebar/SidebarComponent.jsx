ba3f7fdea8135a9d4e09bd089e1ae8d0ae9aacf3
Devansh Gupta <devansh2002gupta@gmail.com>
2025-03-13 23:40:43 +0530

    sidebar
    little update on side bar

--- src/Pages/Sidebar/SidebarComponent.jsx
+++ src/Pages/Sidebar/SidebarComponent.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Drawer, List, ListItem, ListItemText, Box, Grid2 } from "@mui/material";
-import "./SidebarComponent.scss"
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+  Box,
+  Grid2,
+} from "@mui/material";
+import "./SidebarComponent.scss";
+import HomeIcon from "@mui/icons-material/Home";
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 const SidebarComponent = ({ isMobile, open, toggleSidebar }) => {
   return (
@@ -17,10 +26,66 @@ const SidebarComponent = ({ isMobile, open, toggleSidebar }) => {
           className="Drawer"
           onClose={isMobile ? toggleSidebar : undefined}
         >
-          <Box className="DrawerBox" onMouseLeave={!isMobile ? toggleSidebar : undefined}>
-            <List>
-              <ListItem button onClick={toggleSidebar}>
-                <ListItemText primary="Sidebar Content" />
+          <Box
+            className="DrawerBox"
+             onMouseLeave={!isMobile ? toggleSidebar : undefined}
+          >
+            <Box className="SidebarHeader">
+              {/* <ListItemText
+                sx={{
+                  display: "flex",
+                  justifyContent: "start",
+                  alignItems: "center",
+                }}
+                primary="SideBar"
+              /> */}
+              <h4>SideBar</h4>
+              <MenuOpenIcon className="OpenMenuIcon" button onClick={toggleSidebar}  />
+            </Box>
+            <List className="List">
+              <ListItem className="ListItems" button onClick={toggleSidebar}>
+                <HomeIcon />
+                <ListItemText
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                  }}
+                  primary="Home"
+                />
+              </ListItem>
+              <ListItem className="ListItems" button onClick={toggleSidebar}>
+                <HomeIcon />
+                <ListItemText
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                  }}
+                  primary="Home"
+                />
+              </ListItem>
+              <ListItem className="ListItems" button onClick={toggleSidebar}>
+                <HomeIcon />
+                <ListItemText
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                  }}
+                  primary="Home"
+                />
+              </ListItem>
+              <ListItem className="ListItems" button onClick={toggleSidebar}>
+                <HomeIcon />
+                <ListItemText
+                  sx={{
+                    display: "flex",
+                    justifyContent: "start",
+                    alignItems: "center",
+                  }}
+                  primary="Home"
+                />
               </ListItem>
             </List>
           </Box>
